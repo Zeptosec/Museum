41ab2747904ebbcb2aa0a83d8fc92de1ab1397da
Zeptosec <armandasmatkevicius@gmail.com>
2023-10-08 15:46:12 +0300

    Changed something in errorHandler

--- museumback/middleware/errorHandler.ts
+++ museumback/middleware/errorHandler.ts
@@ -9,14 +9,22 @@ export default function errorHandler(
     res: Response,
     next: NextFunction
 ) {
+
     console.log("custom err");
     if (err instanceof ZodError) {
         if (err instanceof ZodError) {
             return res.status(422).json({ errors: formatToErrors(err) });
         }
     } else if (err instanceof TypeError) {
+        console.log("rr1")
         return res.status(400).json({ errors: [err.message] });
+    } else if (err instanceof SyntaxError) {
+        return res.status(422).json({ errors: [err.message] });
     } else if (err instanceof Error) {
+        if (err.message.endsWith('no content-type'))
+            return res.status(422).json({ errors: [err.message] });
+        if (err.message.startsWith('bad content-type'))
+            return res.status(415).json({ errors: [err.message] });
         return res.status(400).json({ errors: [err.message] });
     }
     // this in theory should never get called but just in case
